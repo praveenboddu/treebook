04c8fc2396ba063c61a196a945f10c60731d40b2
Praveen Boddu <praveenkboddu@gmail.com>
2013-03-30 18:36:25 -0400

    added flash messages styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	def flash_class(type)
+		case type
+			when :alert 
+			 	"alert-error"
+			when :notice 
+				"alert-success"
+			else
+			 	"alert-failure"	
+		end	 	
+			
+	end
+
 end
